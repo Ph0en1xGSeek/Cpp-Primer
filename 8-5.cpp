7471451264a779ccb480e0c38bf9ee6c6f3a6532
Ph0en1xGSeek <54panguosheng@gmail.com>
2019-05-21 10:31:04 +0800

    2019.5.21

---
+++ 8-5.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+void read (const string &filename, vector<string> &vec){
+   ifstream ifs(filename);
+   string read_str;
+   while(ifs >> read_str) {
+       vec.push_back(read_str);
+   }
+}
+
+int main() {
+    string filename("./input.txt");
+    vector<string> vec;
+    read(filename, vec);
+    for(string str: vec) {
+        cout << str << endl;
+    }
+    return 0;
+}
