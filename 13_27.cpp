94a8be2bf06067bdd0cb301fc8b59ba17d0d5671
Ph0en1xGSeek <54panguosheng@gmail.com>
2019-11-12 10:39:12 +0800

    2019.11.12

--- 13_27.cpp
+++ 13_27.cpp
@@ -6,27 +6,39 @@
 
 class HasPtr {
 public:
-    HasPtr(const std::string &s = std::string()): ps(new std::string(s)), i(0) { }
-    HasPtr(const HasPtr &ptr) : ps(new std::string(*(ptr.ps))), i(ptr.i) { 
+    HasPtr(const std::string &s = std::string()): ps(new std::string(s)), i(0), use(new std::size_t(1)) { }
+    HasPtr(const HasPtr &ptr) : ps(new std::string(*(ptr.ps))), i(ptr.i), use(ptr.use) { 
         std::cout << "copy" << std::endl;
+        ++*use;
     }
 
     HasPtr& operator=(const HasPtr &ptr) {
         std::cout << "assignment" << std::endl;
-        std::string *new_p = new std::string(*(ptr.ps)); // avoid assigning to itself
-        delete ps;
-        ps = new_p;
+        ++*ptr.use;
+        if(--*use == 0) {
+            delete use;
+            delete use;
+        }
+        use = ptr.use;
+        ps = ptr.ps;
         i = ptr.i;
         return *this;
     }
 
     ~HasPtr() {
         std::cout << "destructor" << std::endl;
-        delete ps;
+        if(--*use == 0) {
+            delete use;
+            delete ps;
+        }
     }
 
     std::string get_ps() {
         return *ps;
+    }
+    
+    void set_ps(std::string str) {
+        *ps = str;
     }
 
     int get_i() {
@@ -35,6 +47,7 @@ public:
 
 private:
     std::string *ps;
+    std::size_t *use;
     int i;
 };
 
@@ -46,6 +59,7 @@ int main() {
     HasPtr hp3("2222");
     std::cout << hp3.get_ps() << " " << hp3.get_i() << std::endl;
     hp3 = hp2;
+    hp2.set_ps("121212121212");
     std::cout << hp3.get_ps() << " " << hp3.get_i() << std::endl;
     return 0;
 }
